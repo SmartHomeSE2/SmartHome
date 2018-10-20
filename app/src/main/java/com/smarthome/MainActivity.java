96c5a6c1b6a6ab082523ce8d39219c14d8f0a848
DanfengTrondset <danfeng.wang0027@stud.hkr.se>
2018-10-20 16:13:02 +0200

    Register page created

--- app/src/main/java/com/smarthome/MainActivity.java
+++ app/src/main/java/com/smarthome/MainActivity.java
@@ -1,12 +1,35 @@
 package com.smarthome;
 
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private static final String TAG = "MainActivity";
+
+    private TextView text_register_login;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i(TAG, "onCreate");
+
+        text_register_login = findViewById(R.id.text_register_login);
+        text_register_login.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == text_register_login) {
+            Log.i(TAG, "register clicked");
+            RegisterFragment registerFragment = new RegisterFragment();
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            fragmentManager.beginTransaction().replace(R.id.layout_login_content_login, registerFragment).addToBackStack(null).commit();
+        }
     }
 }
