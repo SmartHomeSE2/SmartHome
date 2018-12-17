55e68c41f7aa663977b3da3bb0fd77da8ff05262
DanfengTrondset <danfeng.trondset@gmail.com>
2018-12-17 21:42:16 +0100

    sucessufly implemented loginUser, checkDevice and toggleDevice

--- app/src/main/java/com/smarthome/LoginActivity.java
+++ app/src/main/java/com/smarthome/LoginActivity.java
@@ -7,20 +7,28 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+
+import org.apache.commons.lang3.StringUtils;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
     private static final String TAG = "LoginActivity";
 
-    private TextView text_register_login;
+    private EditText edit_email_login;
+    private EditText edit_password_login;
     private Button button_sign_in_login;
+    private TextView text_register_login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         Log.i(TAG, "onCreate");
+
+        edit_email_login = findViewById(R.id.edit_email_login);
+        edit_password_login = findViewById(R.id.edit_password_login);
 
         text_register_login = findViewById(R.id.text_register_login);
         text_register_login.setOnClickListener(this);
@@ -39,8 +47,18 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         }
 
         if (v == button_sign_in_login) {
-            Intent intent = new Intent(this, MainActivity.class);
-            startActivity(intent);
+//            Intent intent = new Intent(this, MainActivity.class);
+//            startActivity(intent);
+
+            String email = edit_email_login.getText().toString();
+            String password = edit_password_login.getText().toString();
+
+            if (StringUtils.isNoneBlank(email, password)) {
+                MyAsync myAsync = new MyAsync();
+                //myAsync.execute("registerUser");
+                //myAsync.execute("checkDevice");
+                myAsync.execute("toggleDevice");
+            }
         }
     }
 }
--- app/src/main/java/com/smarthome/MyAsync.java
+++ app/src/main/java/com/smarthome/MyAsync.java
@@ -4,8 +4,8 @@ import android.content.Context;
 import android.os.AsyncTask;
 import android.util.Log;
 
-import com.google.gson.Gson;
-import com.smarthome.Data.Request;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
@@ -44,47 +44,67 @@ public class MyAsync extends AsyncTask<String, Void, Integer> {
         HttpURLConnection urlConnection;
 
         try {
-            //1. Open a new URL connection
-            URL url = new URL(params[0]);
+            //1. Open a new URL connection]
+            String urlPrefix = "http://83.248.250.216:8000/";
+            String query = params[0];
+            URL url = new URL(urlPrefix + query);
             urlConnection = (HttpURLConnection) url.openConnection();
-            urlConnection.setDoInput(true);
-            //urlConnection.setDoOutput(true);
-/*
+            Log.i(TAG, "urlConnection: " + urlConnection.toString());
+            //urlConnection.setDoInput(true); //by default
+            urlConnection.setDoOutput(true);
+
             //2. Defines a HTTP request type
-            urlConnection.setRequestMethod("GET"); // or POST
+            //urlConnection.setRequestMethod("GET"); // or POST
+            urlConnection.setRequestMethod("POST"); //by default if setDoOutput(true) has been called
 
             //3. Defines a HTTP header
-            urlConnection.setRequestProperty("Accept-Charset", "UTF-8");
+            //urlConnection.setRequestProperty("Accept-Charset", "UTF-8");
             urlConnection.setRequestProperty("Content-Type", "application/json");
+            //urlConnection.setRequestProperty("Accept", "application/json");
             //urlConnection.setRequestProperty("Content-length",//);
-            //urlConnection.setRequestProperty("Content-language", "en-US");
+            urlConnection.setRequestProperty("Content-language", "en-US");
 
             //4. Defines a HTTP body
             //urlConnection.setFixedLengthStreamingMode(contentLength);
             //Post data in JSON format
-           *//* JSONObject jsonObject=new JSONObject();
+            /*Gson gson = new Gson();
+            LoginObj login = new LoginObj("something@something.com", "hej");
+            String loginJson = gson.toJson(login);
+            Log.i(TAG, loginJson);*/
+            JSONObject cred = new JSONObject();
             try {
-                jsonObject.put("id","value");
-            }catch (JSONException e){
-                e.printStackTrace();
-            }*//*
-            Request request = new Request("dbcall", 104, "");
-            Gson gson = new Gson();
-            String requestJson = gson.toJson(request);
-            Log.i(TAG, requestJson);
+                //loginUser
+                /*cred.put("email", "something@something.com");
+                cred.put("password", "hej");*/
 
-            //5. Make the request
+                //checkDevice
+                //cred.put("id","109");
+
+                //toggleDevice
+                cred.put("id","109");
+                cred.put("value","0");
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            urlConnection.connect(); //Operations like getInputStream, getOutputStream, will implicitly invoke connection.connect(), if necessary.
+
+            //5. Write to the request
             OutputStream out = new BufferedOutputStream(urlConnection.getOutputStream());
-            writeStream(out, requestJson);
+            Log.i(TAG, "OurputSteam: " + out.toString());
+            writeStream(out, cred.toString());
+
 
             //6. Read response
             InputStream in = new BufferedInputStream(urlConnection.getInputStream());
-            readStream(in);*/
+            String response = readStream(in);
+            Log.i(TAG, "response: " + response);
 
             //Get response code from the request: 200/500
             int responseCode = urlConnection.getResponseCode();
-            Log.i(TAG, Integer.toString(responseCode));
+            Log.i(TAG, "responseCode: " + Integer.toString(responseCode));
 
+            urlConnection.disconnect();
         } catch (IOException ioe) {
             ioe.printStackTrace();
         }
@@ -104,6 +124,7 @@ public class MyAsync extends AsyncTask<String, Void, Integer> {
         }
 
     }
+
 
     private String readStream(InputStream is) {
         BufferedReader reader = new BufferedReader(new InputStreamReader(is));
@@ -135,7 +156,6 @@ public class MyAsync extends AsyncTask<String, Void, Integer> {
     @Override
     protected void onPostExecute(Integer integer) {
         super.onPostExecute(integer);
-        Log.i(TAG, "Response code: " + integer);
     }
 
 
