4666d4d7610dcfe8165eb60845feb3ed7afe59d3
DanfengTrondset <danfeng.trondset@gmail.com>
2018-12-19 22:03:48 +0100

    clean up and reorganized code

--- app/src/main/java/com/smarthome/NetRequest.java
+++ app/src/main/java/com/smarthome/NetRequest.java
@@ -34,20 +34,18 @@ public class NetRequest extends AsyncTask<String, Void, Integer> {
     @Override
     protected Integer doInBackground(String... params) {
 
-        HttpURLConnection urlConnection;
+        HttpURLConnection urlConnection = null;
 
         try {
-            //1. Open a new URL connection]
+            //1. Obtain a HttpURLConnection object
             String urlPrefix = "http://83.248.250.216:8000/";
-            String query = params[0];
-            URL url = new URL(urlPrefix + query);
+            String path = params[0];
+            URL url = new URL(urlPrefix + path);
             urlConnection = (HttpURLConnection) url.openConnection();
             Log.i(TAG, "urlConnection: " + urlConnection.toString());
 
-            //2. Defines a HTTP request type
-            urlConnection.setRequestMethod("POST"); //by default if setDoOutput(true) has been called
-
-            //3. Defines a HTTP header
+            //2. Defines a HTTP request type and header
+            urlConnection.setRequestMethod("POST");
             urlConnection.setRequestProperty("Charset", "UTF-8");
             urlConnection.setRequestProperty("Accept-Charset", "UTF-8");
             urlConnection.setRequestProperty("Content-Type", "application/json");
@@ -55,55 +53,76 @@ public class NetRequest extends AsyncTask<String, Void, Integer> {
             /*urlConnection.setConnectTimeout(TIMEOUT_CONNECT);
             urlConnection.setReadTimeout(TIMEOUT_READ);*/
 
-            //4. Defines a HTTP body
-            JSONObject cred = new JSONObject();
-            try {
-                //loginUser
-                cred.put("email", "something@something.com");
-                cred.put("password", "hej");
-
-                //checkDevice
-                //cred.put("id","109");
-
-                //toggleDevice
-                /*cred.put("id","109");
-                cred.put("value","0");*/
-
-                //setTemp: Device issue, input value = Celsius + 100, Celsius range -40 ~ 40
-                /*cred.put("id", "110");
-                cred.put("value", "100");*/
-
-                //registerUser: Database issue
-                /*cred.put("userName", "df");
-                cred.put("email", "mobile@mobile.com");
-                cred.put("password", "hi");*/
-                Log.i(TAG, "requestBody: " + cred);
-            } catch (JSONException e) {
-                e.printStackTrace();
+            //3. Defines a HTTP body
+            JSONObject requestJason = setRequestBody(params);
+            if (requestJason == null) {
+                return -1;
             }
 
-            urlConnection.connect(); //Operations like getInputStream, getOutputStream, will implicitly invoke connection.connect(), if necessary.
-
-            //5. Write to the request
+            //4. Write to the request
             OutputStream out = new BufferedOutputStream(urlConnection.getOutputStream());
-            writeStream(out, cred.toString());
+            writeStream(out, requestJason.toString());
 
-
-            //6. Read response
+            //5. Get response code (200/500) and read response
+            int responseCode = urlConnection.getResponseCode();
+            Log.i(TAG, "responseCode: " + Integer.toString(responseCode));
             InputStream in = new BufferedInputStream(urlConnection.getInputStream());
             String response = readStream(in);
             Log.i(TAG, "response: " + response);
-
-            //Get response code from the request: 200/500
-            int responseCode = urlConnection.getResponseCode();
-            Log.i(TAG, "responseCode: " + Integer.toString(responseCode));
-
-            urlConnection.disconnect();
         } catch (IOException ioe) {
             ioe.printStackTrace();
+        } finally {
+            //6. Close the connection
+            if (urlConnection != null) urlConnection.disconnect();
         }
 
         return -1;
+    }
+
+    private JSONObject setRequestBody(String... params) {
+        JSONObject object = new JSONObject();
+        String path = params[0];
+        try {
+            switch (path) {
+                case "loginUser":
+                    String email_login = params[1];
+                    String password_login = params[2];
+                    object.put("email", email_login);
+                    object.put("password", password_login);
+                    break;
+                case "checkDevice":
+                    String id_check = params[1];
+                    object.put("id", id_check);
+                    break;
+                case "toggleDevice":
+                    String id_toggle = params[1];
+                    String value_toggle = params[2];
+                    object.put("id", id_toggle);
+                    object.put("value", value_toggle);
+                    break;
+                case "registerUser":
+                    String userName = params[1];
+                    String email_register = params[2];
+                    String password_register = params[3];
+                    object.put("userName", userName);
+                    object.put("email", email_register);
+                    object.put("password", password_register);
+                case "setTemp":
+                    //setTemp id can be 108 or 110
+                    //Input value = Celsius + 100, Celsius range -40 ~ 40
+                    String id_setTemp = params[1];
+                    String value_setTemp = String.valueOf(Integer.valueOf(params[2]) + 100);
+                    object.put("id", id_setTemp);
+                    object.put("value", value_setTemp);
+                    break;
+                default:
+                    return null;
+            }
+            Log.i(TAG, "requestBody: " + object);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return object;
     }
 
     private void writeStream(OutputStream os, String json) {
@@ -115,16 +134,21 @@ public class NetRequest extends AsyncTask<String, Void, Integer> {
             writer.close();
         } catch (IOException ioe) {
             ioe.printStackTrace();
+        } finally {
+            try {
+                os.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
 
     }
 
-
     private String readStream(InputStream is) {
         BufferedReader reader = new BufferedReader(new InputStreamReader(is));
-        StringBuilder builder = new StringBuilder();
 
         String line;
+        StringBuilder builder = new StringBuilder();
         try {
             while ((line = reader.readLine()) != null) {
                 builder.append(line).append("\n");
